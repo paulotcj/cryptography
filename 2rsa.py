7a7b2174e88f8873325d88af9887a871715e8339
Paulo Costa <paulotcj@gmail.com>
2023-01-25 16:16:28 -0500

    .

--- 2rsa.py
+++ 2rsa.py
@@ -44,11 +44,52 @@
 #     (e*d) = k*φ(n) + 1
 # 	
 # 	d = (k*φ(n) + 1) / e
-
+#--------------------------------------------------------
+#
+# Another example.
+# Define 'p' and 'q'
+# p = 2  ,  q = 7
+# Define n, where n = p*q
+# n = 2*7 = 14
+# Define φ(n) - see explanation above - where φ(n) = (p-1)*(q-1)
+#  φ(n) = (2-1)*(7-1) = (1)*(6) = 6
+# Define 'e'. Where 'e' must be less than φ(n), and be a co-prime with φ(n) and n (e.g.: co prime with 6 and 14)
+#  meaning 'e' should not share any co-factor with φ(n) and n except for 1.
+#  In this case we could have the numbers (2,3,4,5) - and then we eliminate the even numbers, we are left with
+#  (3,5) - and 3 is a co-factor of 6. So our choice is 5
+# e = 5
+# Solve 'd' in the equation (e.d) mod φ(n) = 1. You can pick any 'd'
+#  you could try loop though possible numbers and checking the ones that might solve the equation, e.g.:
+#  (5*1) mod 6 => 5 mod 6 = 1 (OK)
+#  (5*2) mod 6 => 10 mod 6 = 4 (WRONG)
+#  (5*...) mod 6 => ...
+#  (5*11) mod 6 => 55 mod 6 = 1 (OK)
+#  (5*...) mod 6 => ...
+#  (5*17) mod 6 => 85 mod 6 = 1 (OK)
+#  (5*...) mod 6 => ...
+# So in this example the viable options would be 'd' = [5,11,17]
+# We will pick 11
+# d = 11
+# So far the data we have is:
+#   p = 2    , q    = 7
+#   n = 14   , φ(n) = 6
+#   e = 5    , d    = 11
+#
+# Public Keys  (e,n) -> (5 , 14)
+# Private Keys (d,n) -> (11, 14)
+#
+# Consider we want to send a message where A=1, B=2, C=3, ...
+# And the first char is B=2
+# we would calculate:
+#  m^(e) mod n = c  -> 2^(5) mod 14 -> 32 mod 14 = 4 = 'D'
+#
+# And then decrypt using:
+#  m = c^(d) mod n -> 4^(11) mod 14 = 4194304 mod 14 = 2 = 'B'
 
 
 import math
 import random
+import sys
 
 def is_prime(num):
     if num == 2: return True
@@ -70,9 +111,6 @@ def get_prime(min, max):
         if is_prime(randNum):
             return randNum
 
-def lcm(a, b):
-    return a*b//math.gcd(a, b)
-
 
 def get_e(phi_n , n):
     for e in range(2, phi_n):
@@ -81,17 +119,11 @@ def get_e(phi_n , n):
     return False
 
 
-def get_d(e, lambda_n):
-    for d in range(2, lambda_n):
-        if d*e % lambda_n == 1:
+def get_d_min_d(e, phi_n, min_d = 2, max_d = sys.maxsize):
+    for d in range(min_d, max_d):
+        if d*e % phi_n == 1:
             return d
-    return False
-
-
-def factor(n):
-    for p in range(2, n):
-        if n % p == 0:
-            return p, n//p
+    return False    
 
 
 
@@ -107,8 +139,8 @@ q = p
 while q == p:
     q = get_prime(min_value, min_value*2)
 
-p = 2
-q = 7
+# p = 2
+# q = 7
 
 print("   p:", p,", q:", q)
 
@@ -124,22 +156,23 @@ print("Step 4: Choose an integer e such that 1 < e < φ(n) and gcd(e, φ(n)) = 1
 e = get_e(phi_n, n)
 print("   Public exponent:", e)
 
-# print("Step 5: solve for d the equation d⋅e ≡ 1 (mod λ(n))")
-# d = get_d(e, lambda_n)
-# print("   Secret exponent", d)
+print("Step 5: solve for d the equation (d*e) mod φ(n) ≡ 1")
+d = get_d_min_d(e, phi_n, min_d = (e+1))
+print("   Secret exponent", d)
 
-# print("   Done with key generation.")
-# print("   Public key (e,n):", e, n)
-# print("   Secret key (d)", d)
+print("Step 6: Revise the data gathered so far")
+
+print("   Public Keys:  (e,n) -> (", e, "," , n , ")")
+print("   Private Keys: (d)   -> (", d, ")")
 
 
-# print("\nThis is Bob wanting to send a message:")
-# bob_m = 117
-# bob_c = bob_m**e % n
+print("\nThis is Bob wanting to send a message:")
+bob_m = 2
+bob_c = (bob_m**e) % n
 
-# print("Bob's message:",bob_m,"Bob sends:", bob_c)
+print("Bob's message:",bob_m,"Bob's cipher :", bob_c)
 
-# print("This is Alice decrypting the cipher")
-# alice_m = bob_c**d % n
+print("This is Alice decrypting the cipher")
+alice_m = bob_c**d % n
 
-# print("Alice receives this message:", alice_m)
+print("Alice receives this message:", alice_m)
